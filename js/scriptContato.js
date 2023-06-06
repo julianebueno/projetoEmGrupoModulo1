e52d52b0d6e6406732377c1c966e0c2c5e4b28ca
Juliane <julianebueno1@gmail.com>
2023-06-06 17:05:43 -0300

    inclusão javascript do formulario

--- js/scriptContato.js
+++ js/scriptContato.js
@@ -1,36 +1,3 @@
-
-
-
-// function myFunction() {
-//     // Get the value of the input field with id="numb"
-//     let x = document.getElementById("numb").value;
-//     // If x is Not a Number or less than one or greater than 10
-//     let text;
-//     if (isNaN(x) || x < 1 || x > 10) {
-//       text = "Input not valid";
-//     } else {
-//       text = "Input OK";
-//     }
-//     document.getElementById("demo").innerHTML = text;
-//   }
-
-
-
-
-// function validateForm() {
-//     let x = document.forms["myForm"]["fname"].value;
-//     if (x == "") {
-//         alert("Name must be filled out");
-//         return false;
-//     }
-// }
-
-
-
-function validateForm() {
-    let x = document.forms["meuFormulario"]["nome"].value;
-    if (x == "") {
-        alert("Name must be filled out");
-        return false;
-    }
+function validar(){
+    alert("Obrigado pelo seu feedback!") 
 }
