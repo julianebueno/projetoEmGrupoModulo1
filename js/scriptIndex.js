aa81f3803d5a446a4a030e182913f997e3581957
Gabres96 <gabriel.rodrigues670@gmail.com>
2023-06-07 19:07:29 -0300

    Melhorando código js e comentando doc js

--- js/scriptIndex.js
+++ js/scriptIndex.js
@@ -1,43 +1,48 @@
+//-----------------------FUNÇAO COMPRAR PIZZA-------------------------------------//
 function pedirPizza() {
+  alert("PROMO!! Compre 2 ou mais pizzas e ganhe uma broto.")
   var pizza = prompt("Quantas pizzas você deseja?");
   if (pizza >= 2) {
-    alert("Você ganhou um pizza broto!")
-  } else {
+    alert("Você ganhou um pizza broto! Pedido realizado.")
+  } else if (pizza > 0) {
     alert("Pedido realizado com sucesso!")
-    
-  }  
+  }
+
 }
+//------------------------FUNÇAO COMPRAR SORVETE------------------------------------//
 
 function pedirSorvete() {
-    var sorvete = prompt("Quantos sabores você deseja?");
-    if (sorvete >= 3) {
-        alert("Você ganhou a cobertura grátis!")
-    } else {
-        alert("Pedido realizado!")
-    }
+  alert("DESCONTO!! Sorvetes acima de 3 sabores ganha 50% de desconto.")
+  var sorvete = prompt("Quantos sabores você deseja?");
+  if (sorvete >= 3) {
+    alert("Pedido com 50% de desconto realizado! Retire no balcão seu pedido. Obrigado!")
+  } else if (sorvete < 2) {
+    alert("Pedido realizado!")
   }
-  
+}
+//---------------------------FUNCAO COMPRAR CHOPP---------------------------------//
 function pedirChopp() {
-  var chopp = prompt(
-    "Compre 3 chopp e ganhe um grátis.Quantos você deseja comprar?"
-  );
+  alert("GANHE UM CHOPP GRATIS!! comprando 2 o terceiro é Gratis.")
+  var chopp = prompt("Quantos chopps você deseja comprar?");
 
-  if (chopp >= 3) {
-    alert("Você recebeu um chopp grátis!");
-  } else {
+  if (chopp >= 2) {
+    alert("Ganhou Chopp grátis! Pedido realizado.");
+  } else if (chopp > 0) {
     alert("Pedido realizado!");
   }
 }
-
+//-----------------------FUNCAO COMPRAR PASSE-------------------------------------//
 function pedirIdade() {
+  alert("Criança até 5 anos não pagam. Idade maxima permitida 12 anos.")
   var idade = prompt("Quantos anos seu filho tem?");
-  if (idade < 5) {
-    alert("Seu filho precisa de um responsável acompanhando.");
 
-  } else if ( idade > 12) {
+  if (idade <= 5) {
+    alert("Entrada Gratis, apresente o documento na entrada.");
+
+  } else if (idade > 12) {
     alert("Desculpe, idade não permitida.");
-    
-  } else{
-    alert("Entrada permitida.");
+
+  } else {
+    alert("Entrada solicitada! Apresente o documento e realize o pagamento na entrada.");
   }
 }
